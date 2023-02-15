2d88879099ffc28cc23769081330077282a9b6e3
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-02-15 17:30:27 +0000

    player cleanup

--- src/Player.java
+++ src/Player.java
@@ -11,60 +11,56 @@ public class Player {
 
     public static void main(String[] args) {
         Socket socket = null;
-        BufferedReader inputReader;
-        BufferedWriter output;
-        BufferedReader consoleInput;
         try {
             socket = new Socket("localhost", 8080);
-            consoleInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String insertCharName = consoleInput.readLine();
+            System.out.println(insertCharName);
 
-            String insertName = consoleInput.readLine();
-            System.out.println(insertName);
-
-            inputReader = new BufferedReader(new InputStreamReader(System.in));
-            output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             while (!socket.isClosed()) {
                 String msgToSend = inputReader.readLine();
-
                 if (msgToSend.compareTo("EXIT") == 0) {
                     break;
                 }
-
                 output.write(msgToSend);
                 output.newLine();
                 output.flush();
 
-                // Thread creation to always receive messages sent
-                ExecutorService viewMessage = Executors.newCachedThreadPool();
-                Socket finalSocket = socket;
-
-                viewMessage.submit(new Thread(() -> {
-                    String msgReceived = null;
-                    try {
-                        BufferedReader consoleRead = new BufferedReader(new InputStreamReader(finalSocket.getInputStream()));
-                        while (!viewMessage.isShutdown() && (msgReceived = consoleRead.readLine()) != null) {
-                            System.out.println(msgReceived);
-                        }
-                    } catch (EOFException e) {
-                        viewMessage.shutdownNow();
-                        throw new RuntimeException(e);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    } finally {
-                        viewMessage.shutdownNow();
-                    }
-                }));
-
+                receiveBroadcast(socket);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
             try {
-                System.out.println(Colors.BLUE + "You have been disconnected from the server" + Colors.RESET);
+                System.out.println(Colors.RED + "You have been disconnected from the server" + Colors.RESET);
                 socket.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    private static void receiveBroadcast(Socket socket) {
+        ExecutorService viewMessage = Executors.newCachedThreadPool();
+        Socket finalSocket = socket;
+
+        viewMessage.submit(new Thread(() -> {
+            String msgReceived = null;
+            try {
+                BufferedReader consoleRead = new BufferedReader(new InputStreamReader(finalSocket.getInputStream()));
+                while (!viewMessage.isShutdown() && (msgReceived = consoleRead.readLine()) != null) {
+                    System.out.println(msgReceived);
+                }
+            } catch (EOFException e) {
+                viewMessage.shutdownNow();
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                viewMessage.shutdownNow();
+            }
+        }));
     }
 }
