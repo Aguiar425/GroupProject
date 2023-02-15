6fb3ba47e9098cda5d0fe09a5f85747d55a544a4
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-02-15 15:06:04 +0000

    gitIgnore added

--- src/Field/Field.java
+++ src/Field/Field.java
@@ -1,32 +1,65 @@
 package Field;
 
-import javax.swing.*;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
 
 public class Field {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("frame");
-        frame.setSize(1200,800);
 
-        Grid grid = new Grid();
-        frame.add(grid);
-        frame.setVisible(true);
+        // Setup terminal and screen layers
+        Terminal terminal = null;
+        Screen screen;
+        try {
+            terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.startScreen();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Create panel to hold components
+        Panel panel = new Panel();
+        panel.setLayoutManager(new GridLayout(2));
+
+        panel.addComponent(new Label("Forename"));
+        panel.addComponent(new TextBox());
+
+        panel.addComponent(new Label("Surname"));
+        panel.addComponent(new TextBox());
+
+        panel.addComponent(new EmptySpace(new TerminalSize(0, 0))); // Empty space underneath labels
+        panel.addComponent(new Button("Submit"));
+
+        // Create window to hold the panel
+        BasicWindow window = new BasicWindow();
+        window.setComponent(panel);
+
+        // Create gui and start gui
+        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.RED));
+        gui.addWindowAndWait(window);
+
     }
-    public static void startScreen(int width, int height) {
 
-        screen = TerminalFacade.createScreen();
 
-        Field.width = width;
-        Field.height = height;
-        screen.getTerminal().getTerminalSize().setColumns(width);
-        screen.getTerminal().getTerminalSize().setRows(height);
+    public void Field() {
+        TerminalSize size = new TerminalSize(80, 50); // set the desired size of the terminal
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(size);
+        try {
+            Terminal terminal = terminalFactory.createTerminal(); // create the terminal with the specified size
+            // do something with the terminal
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
 
-        screenWriter = new ScreenWriter(screen);
-        screen.setCursorPosition(null);
-        screen.startScreen();
-
-        drawWalls();
-        screen.refresh();
     }
+
+
 }
--- src/Game.java
+++ src/Game.java
@@ -1,91 +1,125 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Game {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String BLUE = "\033[34m";
+    public static final String YELLOW = "\033[33m";
+    public static final String PINK = "\u001B[35m";
+    private static List<Socket> clientConnections = new ArrayList<>();
+    private static HashMap<String, Socket> clientMap = new HashMap<>();
 
-    private ServerSocket gameSocket;
-    private ExecutorService service;
-    //private Character[] party;
-    private Party party;
-    private Party party2;
+    public static void main(String[] args) {
 
-    private BufferedReader input;
-    private BufferedWriter output;
+        ServerSocket serverSocket;
+        try {
+            serverSocket = new ServerSocket(8080);
+            writeHistory("\nNEW SESSION");
 
-    public Game() throws IOException {
-        /*gameSocket = new ServerSocket(8082);
-        this.input = new BufferedReader(new InputStreamReader(System.in));
-        this.output = new BufferedWriter(new OutputStreamWriter(gameSocket.getOutputStream()));*/
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many players?");
-        //this.party = new Character[scanner.nextInt()];
-        this.gameSocket = gameSocket;
-    }
+            while (true) {
+                ExecutorService chat = Executors.newCachedThreadPool();
+                final Socket clientSocket = serverSocket.accept();
+
+                BufferedReader consoleInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter outputName = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                outputName.write(YELLOW + "Please insert your username (" + RED + "EXIT" + YELLOW + " to leave the room, and " + PINK + "@+username" + YELLOW + " to send pm)" + RESET);
+                outputName.newLine();
+                outputName.flush();
+
+                String userName = consoleInput.readLine();
+                broadcastMessage(BLUE + userName.concat(" has joined the server") + RESET);
+                System.out.println(BLUE + userName.concat(" has joined the server" + RESET).toUpperCase());
+                writeHistory(userName.concat(" has joined the server").toUpperCase());
 
 
-    public void startGame() throws IOException {
-        gameSocket = new ServerSocket(8082);
-        service = Executors.newCachedThreadPool();
-
-        while (true) {
-            acceptConnection();
-        }
-    }
-
-    public void acceptConnection() throws IOException {
-        Socket clientSocket = gameSocket.accept();
-        CharacterConnectionHandler characterConnectionHandler =
-                new CharacterConnectionHandler(clientSocket,
-                        "temporary" );
-        service.submit(characterConnectionHandler);
-        //addClient(characterConnectionHandler);
-    }
-
-    public class CharacterConnectionHandler implements Runnable {
-
-        private String name;
-        private Socket clientSocket;
-        private BufferedWriter out;
-        private String message;
-
-        public CharacterConnectionHandler(Socket clientSocket, String name) throws IOException {
-            this.clientSocket = clientSocket;
-            this.name = name;
-            this.out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-        }
-
-        @Override
-        public void run() {
-
-        }
-
-       /* @Override
-        public void run() {
-            addClient(this);
-            try {
-                // BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                Scanner in = new Scanner(clientSocket.getInputStream());
-                while (in.hasNext()) {
-                    message = in.nextLine();
-                    if (isCommand(message)) {
-                        dealWithCommand(message);
-                        continue;
+                Thread t = new Thread(() -> {
+                    try {
+                        newClient(userName, clientSocket, clientConnections, clientMap);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
                     }
-                    if (message.equals("")) {
-                        continue;
-                    }
-
-                    broadcast(name, message);
-                }
-            } catch (IOException e) {
-                System.err.println(messages.CLIENT_ERROR + e.getMessage());
-            } finally {
-                removeClient(this);
+                });
+                chat.submit(t);
             }
-        }*/
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static void newClient(String user, Socket socket, List<Socket> socketList, HashMap<String, Socket> clientMap) throws IOException {
+        socketList.add(socket);
+        clientMap.put(user, socket);
+        System.out.println(RED + socketList + RESET);
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String msgReceived;
+
+        while ((msgReceived = inputReader.readLine()) != null) {
+            if (msgReceived.startsWith("@")) {
+                msgReceived = msgReceived.substring(1);
+                privateMessage(socket, clientMap, msgReceived, user);
+
+            } else {
+                System.out.println("Message received from " + user.concat(": " + msgReceived));
+                broadcastMessage(user + ": " + msgReceived);
+                writeHistory(user.concat(": " + msgReceived));
+            }
+        }
+        socketList.remove(socket);
+        clientMap.remove(user, socket);
+        broadcastMessage(YELLOW + user.concat(" has left the server").toUpperCase() + RESET);
+        writeHistory(user.concat(" has left the server").toUpperCase());
+        System.out.println(YELLOW + user.concat(" has left the server").toUpperCase() + RESET);
+        System.out.println(RED + socketList + RESET);
+    }
+
+    private static void privateMessage(Socket socket, HashMap<String, Socket> clientMap, String pmName, String user) throws IOException {
+        BufferedReader inputPM = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String pmReceived = inputPM.readLine();
+
+        for (Map.Entry<String, Socket> set : clientMap.entrySet()) {
+            if (set.getKey().compareTo(pmName) == 0) {
+                BufferedWriter outPut = new BufferedWriter(new OutputStreamWriter(set.getValue().getOutputStream()));
+                outPut.write(PINK + user.concat(" PM: ").concat(pmReceived) + RESET);
+                outPut.newLine();
+                outPut.flush();
+                return;
+            }
+        }
+        BufferedWriter outPut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        outPut.write(RED + "User doesn't exist" + RESET);
+        outPut.newLine();
+        outPut.flush();
+
+    }
+
+    private static void broadcastMessage(String message) throws IOException {
+        for (Socket client : clientConnections) {
+            BufferedWriter outPut = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            outPut.write(message);
+            outPut.newLine();
+            outPut.flush();
+        }
+    }
+
+    private static void writeHistory(String writeThis) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("serverHistory.txt", true));
+
+            writer.write(writeThis);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
+
     }
 }
