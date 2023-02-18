659b8a0785693d013bffcd5b07f295658da7ca70
Aguiar425 <aguiar425@gmail.com>
2023-02-18 20:45:14 +0000

    attack randomizer done

--- src/messages/commands/battleCommands/AttackCommand.java
+++ src/messages/commands/battleCommands/AttackCommand.java
@@ -3,18 +3,21 @@ package messages.commands.battleCommands;
 import Game.Game;
 import Game.GameServer;
 import gameObjects.PlayerCharacter;
+import gameObjects.RandomNumber;
 import messages.Colors;
 
 import java.io.IOException;
 
 
-public class AttackCommand implements BattleHandler { //TODO RANDOMIZE DAMAGE
+public class AttackCommand implements BattleHandler {
     @Override
     public void execute(GameServer server, Game game, PlayerCharacter character) throws IOException {
 
-        game.getAllMonsters().setHitpoints(game.getAllMonsters().getHitpoints() - character.getMinDamage());
-        System.out.println(character.getName().concat(" dealt ") + Colors.BLUE + character.getMinDamage() + Colors.RESET + " of damage!");
-        server.broadcastMessage(character.getName().concat(" dealt ") + Colors.BLUE + character.getMinDamage() + Colors.RESET + " of damage!");
+        int damage = RandomNumber.randomizer(character.getMinDamage(), character.getMaxDamage());
+
+        game.getAllMonsters().setHitpoints(game.getAllMonsters().getHitpoints() - damage);
+        System.out.println(character.getName().concat(" dealt ") + Colors.BLUE + damage + Colors.RESET + " of damage!");
+        server.broadcastMessage(character.getName().concat(" dealt ") + Colors.BLUE + damage + Colors.RESET + " of damage!");
         System.out.println(game.getAllMonsters() + " has " + game.getAllMonsters().getHitpoints() + " hitpoints");
     }
 }
