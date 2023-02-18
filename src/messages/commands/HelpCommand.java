c56f5d13fd6199f35a41eecdf16d695dd87b3940
Aguiar425 <aguiar425@gmail.com>
2023-02-18 21:17:22 +0000

    finished HELP and LIST commands

--- src/messages/commands/HelpCommand.java
+++ src/messages/commands/HelpCommand.java
@@ -3,11 +3,21 @@ package messages.commands;
 import Game.Game;
 import Game.GameServer;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class HelpCommand implements CommandHandler {
     @Override
-    public void execute(GameServer server, Game game, Socket socket, String name) {
+    public void execute(GameServer server, Game game, Socket socket, String name) throws IOException {
+
+        server.writeAndSend(socket, "These are the available commands");
+        server.writeAndSend(socket, "/help  -> Lists all commands");
+        server.writeAndSend(socket, "/save  -> Saves the game");
+        server.writeAndSend(socket, "/exit  -> Leaves the game");
+        server.writeAndSend(socket, "/back  -> Returns the party to the previous screen");
+        server.writeAndSend(socket, "/list  -> Lists all players in the game");
+        server.writeAndSend(socket, "/inv  -> Shows the party's inventory");
+        server.writeAndSend(socket, "/stats  -> Shows your character stats");
 
     }
 }
--- src/messages/commands/ListCommand.java
+++ src/messages/commands/ListCommand.java
@@ -2,12 +2,18 @@ package messages.commands;
 
 import Game.Game;
 import Game.GameServer;
+import gameObjects.PlayerCharacter;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class ListCommand implements CommandHandler {
     @Override
-    public void execute(GameServer server, Game game, Socket socket, String name) {
+    public void execute(GameServer server, Game game, Socket socket, String name) throws IOException {
 
+        for (PlayerCharacter pc :
+                game.getParty()) {
+            server.writeAndSend(socket, pc.getName() + " - " + pc.getCharacterClass().getDescription());
+        }
     }
 }
