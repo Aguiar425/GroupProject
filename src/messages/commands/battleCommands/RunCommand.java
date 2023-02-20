0103c700d65802a12e80420479ef0ce24faefd6d
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-02-20 10:45:46 +0000

    run command fixed

--- src/messages/commands/battleCommands/RunCommand.java
+++ src/messages/commands/battleCommands/RunCommand.java
@@ -4,6 +4,7 @@ import Game.Game;
 import Game.GameServer;
 import gameObjects.PlayerCharacter;
 import gameObjects.RandomNumber;
+import messages.Colors;
 import messages.Messages;
 
 import java.io.IOException;
@@ -12,7 +13,10 @@ public class RunCommand implements BattleHandler {
     @Override
     public void execute(GameServer server, Game game, PlayerCharacter character) throws IOException {
         int chance = RandomNumber.randomizer(1, 2);
-
+        if (game.getCurrentRoom() == 24) {
+            server.broadcastMessage(Colors.RED + "                      HAHAHAHAH, YOU THINK YOU CAN ESCAPE ME???????" + Colors.RESET);
+            server.broadcastMessage(Colors.RED + "                                      THINK AGAIN" + Colors.RESET);
+        }
         switch (chance) {
             case 1:
                 game.setInCombat(false);
